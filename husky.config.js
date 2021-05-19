c558ab56917b33021a5c9b426ab99c32b1c2520a
Ivo Dolenc <ivodolenc@gmail.com>
2021-05-19 21:24:16 +0200

    chore: update 'husky' to 6.0.0

--- husky.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  hooks: {
-    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit': 'yarn lint:eslint && yarn lint:stylelint',
-    'pre-push': 'yarn lint:eslint && yarn lint:stylelint'
-  }
-}
